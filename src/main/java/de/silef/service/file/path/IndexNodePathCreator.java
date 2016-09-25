5572d38306f0b2532223bbe0dec93e31ca95e722
Xemle <xemle@phtagr.org>
2016-09-25 19:24:17 +0200

    Hardening data for index importer

--- src/main/java/de/silef/service/file/path/IndexNodePathCreator.java
+++ src/main/java/de/silef/service/file/path/IndexNodePathCreator.java
@@ -1,9 +1,13 @@
 package de.silef.service.file.path;
 
 import de.silef.service.file.node.IndexNode;
-import de.silef.service.file.tree.*;
+import de.silef.service.file.tree.SuppressErrorPathVisitor;
+import de.silef.service.file.tree.Visitor;
+import de.silef.service.file.tree.VisitorChain;
+import de.silef.service.file.tree.VisitorFilter;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 /**
@@ -18,6 +22,11 @@ public class IndexNodePathCreator {
     }
 
     public IndexNode create(Path base, CreatePathFilter pathFilter) throws IOException {
+        if (!Files.isDirectory(base)) {
+            throw new IOException("Base path must be a directory");
+        } else if (Files.isSymbolicLink(base)) {
+            throw new IOException("Base path must not a symbolic link");
+        }
         Visitor<Path> resolveLinkVisitor = new ResolveLinkVisitorFilter(base);
         Visitor<Path> filterVisitor = new VisitorFilter<>(pathFilter::isValidPath);
         IndexNodePathVisitor nodeVisitor = new IndexNodePathVisitor(pathFactory);
@@ -26,6 +35,10 @@ public class IndexNodePathCreator {
         Visitor<Path> suppressErrorVisitor = new SuppressErrorPathVisitor<>(visitorChain);
         PathWalker.walk(base, suppressErrorVisitor);
 
+        IndexNode root = nodeVisitor.getRoot();
+        if (root == null) {
+            throw new IOException("Index root node is empty");
+        }
         return nodeVisitor.getRoot();
     }
 }
